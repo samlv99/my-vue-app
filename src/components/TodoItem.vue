5e96f9f20878763907c793cf6f3ff65d7c681e9c
samlv99 <samlv3112@gmail.com>
2021-07-15 00:31:13 +0700

    refs styles

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <p :class="['todo-item', todoProps.completed ? 'is-completed' : '']" :id="new_id">
+      <input type="checkbox" :checked="todoProps.completed">
+      {{ todoProps.title }}
+      <button class="del-btn">Delete</button>
+  </p>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+    name: 'TodoItem',
+    props: ['todoProps'],
+    setup() {
+        const new_id = ref('my-new-id');
+        return {
+            new_id
+        }
+    }
+}
+</script>
+
+<style>
+.todo-item {
+    background: #f4f4f4;
+    padding: 20px;
+    margin: 0;
+    border-bottom: 1px #ccc solid;
+}
+
+.is-completed {
+    text-decoration: line-through;
+}
+
+.del-btn {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    float: right;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,35 @@
 <template>
-  <p v-for="todo in todos" v-bind:key="todo">{{todo}}</p>
+  <TodoItem v-for="todo in todos" v-bind:key="todo.id" v-bind:todoProps="todo" />
 </template>
 
 <script>
 import {ref} from 'vue'
+import TodoItem from './TodoItem.vue';
 
 export default {
     name: 'Todos',
+    components: {
+        TodoItem,
+    },
+
     setup() {
-        const todos = ref(['Việc 1', 'Việc 2', 'Việc 3'])
+        const todos = ref([
+            {
+                id: 1,
+                title: 'Việc 1',
+                completed: true
+            },
+            {
+                id: 2,
+                title: 'Việc 2',
+                completed: true
+            },
+            {
+                id: 3,
+                title: 'Việc 3',
+                completed: true
+            },
+        ])
         return {
             todos,
         }
