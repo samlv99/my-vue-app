06cdf733a34080d65476fedc917399fb43f8f313
samlv99 <samlv3112@gmail.com>
2021-07-14 23:30:44 +0700

    refs #1 create vue my ap

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <p v-for="todo in todos" v-bind:key="todo">{{todo}}</p>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+    name: 'Todos',
+    setup() {
+        const todos = ref(['Việc 1', 'Việc 2', 'Việc 3'])
+        return {
+            todos,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
